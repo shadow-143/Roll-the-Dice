5f2a96a45d11fc7cf99cc7858e858e4cc35801c4
shadow-143 <108011180+shadow-143@users.noreply.github.com>
2023-01-28 11:22:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//Image 1
+
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomImage1="dice"+randomNumber1+".png";
+
+var randomImage1Source="images/"+randomImage1;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImage1Source);
+
+
+//Image 2
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImage2="dice"+randomNumber2+".png";
+
+var randomImage2Source="images/"+randomImage2;
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImage2Source);
+
+
+//Conditions
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player1 Wins!";
+}
+else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML="Match Draw!";
+}
+else{
+    document.querySelector("h1").innerHTML="Player2 Wins!";
+}
